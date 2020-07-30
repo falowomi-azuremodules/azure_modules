82a3c1abb9290500ecfbd5e313a6ebbaa83dbbac
falowomi <57497870+falowomi@users.noreply.github.com>
2020-07-30 12:02:40 -0500

    Update index.php

--- bin/index.php
+++ bin/index.php
@@ -1,5 +1,14 @@
+#!/bin/bash
+apt-get update -y
+apt-get -y install apache2 php7.0 libapache2-mod-php7.0
+service apache2 start
+
+cat <<EOF > /var/www/html/index.php
 <?php
-$hostname = gethostname();
+\$hostname = gethostname();
 echo "<h2>Hello World!</h2>";
 echo "<h2>Server Hostname: $hostname</h2>";
 ?>
+EOF
+
+service apache2 restart
