7ab32e76f3d0aa2e3e096233bd0ffe97af19954c
falowomi <57497870+falowomi@users.noreply.github.com>
2020-08-01 13:06:10 -0500

    Update auto_script.sh

--- auto_script.sh
+++ auto_script.sh
@@ -2,8 +2,19 @@
 apt-get update -y
 apt-get -y install apache2 php7.0 libapache2-mod-php7.0
 service apache2 start
+service apache2 stop
 
-cat <<EOF > /var/www/html/index.php
+
+mkdir site
+mkdir site/main
+mkdir site/main/html
+
+#sed -i 's+DocumentRoot /var/www/html+DocumentRoot /site/main/html+g' /etc/apache2/sites-enabled/000-default.conf
+#sed -i 's+<Directory /var/www>+<Directory /site/main>+g' /etc/apache2/apache2.conf
+
+apachectl configtest
+
+cat <<EOF > /site/main/html/index.php
 <?php
 \$hostname = gethostname();
 echo "<h2>Hello World!</h2>";
@@ -11,6 +22,6 @@ echo "<h2>Server Hostname: \$hostname</h2>";
 ?>
 EOF
 
-rm /var/www/html/index.html
+rm -rf /var/www
 
-service apache2 restart
+service apache2 start
